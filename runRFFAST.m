ac3d93a87dcfaf0a42a61cafd244c01f8d911a52
tachwali <y.tachwali@gmail.com>
2016-01-03 15:03:01 -0800

    add close and clear and fix title bug

--- runRFFAST.m
+++ runRFFAST.m
@@ -1,7 +1,9 @@
+clear all; close all;
 create_test_signal;
 subplot(2,1,1);
-title('fft(x)')
 plot(abs(fft(x)));
+title('fft(x)');
 subplot(2,1,2);
-title('RFFAST(x)')
-plot(abs(RFFAST(x)))
+plot(abs(RFFAST(x)));
+title('RFFAST(x)');
+
